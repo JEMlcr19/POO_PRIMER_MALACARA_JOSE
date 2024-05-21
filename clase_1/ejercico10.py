510ee6c9401d465c22876029282a60177738af32
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-21 08:00:50 -0600

    Programa 10 - hasta que ponga n

---
+++ clase_1/ejercico10.py
@@ -0,0 +1,12 @@
+respuesta = "n"
+while True: 
+    num1 = str(input("Dame un número: "))
+    num2 = str(input("Dame un número: "))
+    suma = num1 + num2
+    if num1 == respuesta:
+        break
+    else:
+        print("La suma es:",suma)
+
+
+
