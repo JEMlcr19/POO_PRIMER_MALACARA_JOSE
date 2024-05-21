6ac94781c1f5ff0cd86e45a37bd24d81774e1e29
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-21 09:20:26 -0600

    Programa 10 - hasta que ponga n

--- clase_1/ejercico10.py
+++ clase_1/ejercico10.py
@@ -1,12 +1,19 @@
 respuesta = "n"
 while True: 
-    num1 = str(input("Dame un número: "))
-    num2 = str(input("Dame un número: "))
-    suma = num1 + num2
-    if num1 == respuesta:
+    print("1. Hacer una suma")
+    print("2. Salir del programa")
+
+    opcion = int(input("Ingresa el número de la opción que deseas realizar: "))
+
+    if opcion == 2:
+        print("¡Haz salido del programa!")
         break
-    else:
-        print("La suma es:",suma)
+
+    num1 = float(input("Dame el primer número: "))
+    num2 = float(input("Dame el segundo número: "))
+    suma = num1 + num2
+  
+    print("La suma es:",suma)
 
 
 
