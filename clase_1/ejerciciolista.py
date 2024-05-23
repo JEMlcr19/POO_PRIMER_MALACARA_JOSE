36abf3fbd1cc885ca27904ae94df2d4c34a06368
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-23 07:39:38 -0600

    Notas de clase - Listas

---
+++ clase_1/ejerciciolista.py
@@ -0,0 +1,10 @@
+#lista = [1,2,"Jose", 3,4,True]
+#lista[3] = "Eduardo"
+#lista.insert(4,"Malacara")
+#lista.append("Ibarra")
+lista = [1,2,3]
+lista.extend([4,5,6])
+#lista.remove(4)
+elemento = lista.pop(3)
+print(elemento)
+print(lista)
