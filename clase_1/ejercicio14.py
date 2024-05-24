82e4bc84cc515c9388eb112751c4b88a013c915b
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-24 07:49:27 -0600

    Programa 14 - for operación

---
+++ clase_1/ejercicio14.py
@@ -0,0 +1,9 @@
+#Escribe un programa que sume todos los elementos de una lista y muestre el resultado
+
+numeros = [10, 12, 13, 11, 14, 15]
+suma_total = 0
+
+for numero in numeros:
+    suma_total += numero
+    
+print("La suma de los numero es:",suma_total)
