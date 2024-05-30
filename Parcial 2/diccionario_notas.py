59e7644f5691cab4472362345f485126e330c882
JEMlcr19 <jeduardomalacara@gmail.com>
2024-05-30 07:39:51 -0600

    Apuntes de clase sobre uso de diccionario

---
+++ Parcial 2/diccionario_notas.py
@@ -0,0 +1,14 @@
+diccionario = {  #crear un diccionario
+    "nombre":"José",
+    "edad":18,
+    "ciudad":"Durango"
+}
+
+#diccionario = dict(nombre="José", pais = "México", casas=4)   #otra forma de crear un diccionario
+#print(diccionario["nombre"])    #Imprimir un solo valor del diccionario
+#diccionario["nombre"]="Eduardo" #Cambiar un valor existente
+#diccionario["carrera"] = "TI" #crear nuevo valor
+#del diccionario["ciudad"]  #Imprime un valor de la lista
+print(diccionario.keys()) #imrprime las claves de las variables del diccionario
+print(diccionario.values())  #imprime los valores de la variable
+print(diccionario.items())
