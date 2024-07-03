18884e10bc0299ccaeac99505563bcb90e529b4a
JEMlcr19 <jeduardomalacara@gmail.com>
2024-07-02 23:06:07 -0600

    Programa 23-clases y objetos

--- Parcial_2/ejercicio23.py
+++ Parcial_2/ejercicio23.py
@@ -1,54 +1,23 @@
-#Definir la clase
-class Estudiante:  #Nombre de la clase
-    def __init__(self, nombre, carrera, matricula):
-        self.nombre = nombre #Atributo
-        self.carrera = carrera #Atributo
-        self.matricula = matricula #Atributo
+#clase carro
+class Carro:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
     
-    def estudiar(self): #Metodo 1
-        print("Estudiando Ando")
-    def faltar(self): #Metodo 2
-        print("Se salto la clase")
-    def examn(self): #Metodo 3
-        print("El alumno esta presentando examen")
+    def rodando(self):
+        print("Sali a dar el rol",self.modelo,"run, run")
 
-class Horario:
-    def __init__(self, materia, hora):
-        self.materia = materia
-        self.hora = hora
+    def prender(self):
+        print("Prendiendo",self.modelo,"arrancando")
+    
+    def apagarme(self):
+        print(self.modelo,"Me voy a ir a dormir")
 
-#Definir Objetos
-alumno1 = Estudiante("Jose", "LANEV", 68821)
-alumno2 = Estudiante("Jorge", "LAN", 68848)
+#objetos clase carro
+carro1 = Carro("Honda", "Accord", 2024)
+carro2 = Carro("Porsche", "911", 2024)
 
-#Mostrar los atributos del objeto
-print(alumno1.nombre, alumno1.carrera), alumno1.examn()
-print(alumno2.nombre, alumno2.matricula), alumno2.faltar() #El metodo de la clase para mi objeto
-
-
-class Deporte:
-    def _init_(self, nombre, duracion):
-        self.nombre = nombre
-        self.duracion = duracion
-    def entrenando(self):
-        print("Entrenando mi deporte")
-
-    def escuchar(self):
-        print("Escuchando musica ideal para el entreno")
-
-class Entrenamiento:
-    def _init_(self, nombre, tipo):
-        self.nombre = nombre
-        self.tipo = tipo
-        self.entrenamientos = []
-
-    def ir_a_entrenar(self, deporte):
-        self.ir_a_entrenar(deporte)
-        print(f"{self.nombre} entreno {Entrenamiento.nombre}")
-
-    def entrenamiento_realizad(self):
-        print(f"{self.nombre} ha realizado estos entrenos en la semana:")
-        for entrenamiento in self.entrenamientos:
-            print(entrenamiento.nombre)
-
-
+#Usar metodos
+carro1.rodando()
+carro2.prender()
