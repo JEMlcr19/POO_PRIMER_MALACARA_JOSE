95e51bd93e7143332378f5577d9877ae9b1ffd0d
JEMlcr19 <jeduardomalacara@gmail.com>
2024-07-02 22:29:04 -0600

    Programa 21 – Tupla desempaquetada

---
+++ Parcial_2/ejercicio21.py
@@ -0,0 +1,17 @@
+#Crea un programa que contenga una tupla con información de una persona y
+#desempaqueta la información en 5 diferentes variables para imprimirlas por
+#separado (por ejemplo nombre, edad…).
+
+#Tupla
+datos_persona = ("José Malacara",24,"Granja JEMA","UTD","jose@jose.com")
+
+#desempaquetar
+
+nombre, edad, direccion, escuela, correo = datos_persona
+
+#imprimr datos
+print("Nombre:",nombre)
+print("Edad:",edad)
+print("Dirección:",direccion)
+print("Escuela:",escuela)
+print("Correo:",correo)
